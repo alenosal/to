83fa7877bbfd5eb88e43c87b1f3387d26ee5af30
alenosal <milosz@LAPTOP-BC7EUGUQ.localdomain>
2019-11-23 17:19:01 +0100

    Singleton Patter

---
+++ Singleton/Singleton/src/main/java/com/mycompany/singleton/Singleton.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.singleton;
+
+/**
+ *
+ * @author milos
+ */
+
+ 
+public class Singleton {
+    private volatile static Singleton instance;
+    
+    private Singleton() {
+    }
+    
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        
+        return instance;
+    }
+    
+}
