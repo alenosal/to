6cc01f65ffa0bb08812281bcfc91082dfc9474cb
Student Domena <dstudent@ii.local>
2019-11-27 18:32:46 +0100

    TO projekt

---
+++ Projekt_to_Aleksander_Nosal/src/main/java/com/mycompany/projekt_to_aleksander_nosal/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.projekt_to_aleksander_nosal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
